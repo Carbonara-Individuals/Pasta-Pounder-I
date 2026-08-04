128b49e9d5d5e9e0f63ea710357f6d36351d41c2
Turtlerock0010 <danielrprincipe@gmail.com>
2026-08-03 23:53:45 -0400

    Put credits in code

--- software/Pasta-Pounder-I/Pasta-Pounder-I.ino
+++ software/Pasta-Pounder-I/Pasta-Pounder-I.ino
@@ -1,3 +1,14 @@
+/*
+Program: Pasta Pounder I Code
+Creation: July 1st, 2026
+Contributors: Daniel Principe, Saurya Shrestha
+Team: Carbonara Individuals
+Use: The code that goes into Pasta Pounder I
+Microprocessor: ESP32-S3-WROOM-1U
+Dependencies: DRV8874 Lib, Alfredo CRSF
+*/
+
+
 void setup() {
   // Initialize serial communication for debugging
   Serial.begin(115200);
