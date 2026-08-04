c25563675152d18f14323e2b0fd5804066176ff8
Turtlerock0010 <danielrprincipe@gmail.com>
2026-08-04 00:03:43 -0400

    Test commit of verification checks

--- software/Pasta-Pounder-I/Pasta-Pounder-I.ino
+++ software/Pasta-Pounder-I/Pasta-Pounder-I.ino
@@ -5,7 +5,7 @@ Contributors: Daniel Principe, Saurya Shrestha
 Team: Carbonara Individuals
 Use: The code that goes into Pasta Pounder I
 Microprocessor: ESP32-S3-WROOM-1U
-Dependencies: DRV8874 Lib, Alfredo CRSF
+Dependencies: DRV8874 Lib, AlfredoCRSF 
 */
 
 
