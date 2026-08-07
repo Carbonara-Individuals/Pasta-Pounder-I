1785610b78b2c074da6b65007cdfed95e3b72989
Turtlerock0010 <danielrprincipe@gmail.com>
2026-08-07 18:52:27 -0400

    Added initial dependencies

--- software/Pasta-Pounder-I/Pasta-Pounder-I.ino
+++ software/Pasta-Pounder-I/Pasta-Pounder-I.ino
@@ -5,9 +5,11 @@ Contributors: Daniel Principe, Saurya Shrestha
 Team: Carbonara Individuals
 Use: The code that goes into Pasta Pounder I
 Microprocessor: ESP32-S3-WROOM-1U
-Dependencies: DRV8874 Lib, AlfredoCRSF 
+Dependencies: Carbonara_DRV8874, AlfredoCRSF 
 */
 
+#include <AlfredoCRSF.h>
+#include <Carbonara_DRV8874.h>
 
 void setup() {
   // Initialize serial communication for debugging
